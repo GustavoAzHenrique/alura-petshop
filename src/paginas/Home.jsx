c69370f16579b38657ad180d16fda04eebe89b3b
GustavoAzHenrique <92117675+GustavoAzHenrique@users.noreply.github.com>
2022-08-24 17:39:07 -0300

    implementações aula 08

--- src/paginas/Home.jsx
+++ src/paginas/Home.jsx
@@ -1,12 +1,14 @@
 import React from "react";
+import ListaCategorias from "../components/ListaCategorias";
 import ListaPost from "../components/ListaPost";
 
 const Home = () => {
 	return (
 		<main>
 			<div className="container">
-				<h2 className="titulo-pagina">Pet notícias</h2>
+				<h2 className="titulo-pagina">Pet notícias:</h2>
 			</div>
+			<ListaCategorias />
 			<ListaPost url={"/posts"} />
 		</main>
 	);
--- src/paginas/Post.jsx
+++ src/paginas/Post.jsx
@@ -1,19 +1,23 @@
 /* eslint-disable no-undef */
 
 import React, { useState, useEffect } from "react";
-import { useParams } from "react-router-dom";
+import { useParams, useHistory } from "react-router-dom";
 
 import { busca } from "../Api/api";
 
 import "../assets/css/post.css";
 
 const Post = () => {
+	let history = useHistory();
 	const { id } = useParams();
 
 	const [posts, setPosts] = useState({});
 
 	useEffect(() => {
-		busca(`/posts/${id}`, setPosts);
+		busca(`/posts/${id}`, setPosts).catch(() => {
+			history.push("/404");
+		});
+		// eslint-disable-next-line react-hooks/exhaustive-deps
 	}, [id]);
 
 	return (
